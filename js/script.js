9489651caa21d85324d566b3cc3546868b4a4925
oleksii.chernii <oleksii.chernii@gmail.com>
2025-10-15 21:57:35 +0300

    added brands section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
-const buttons = document.querySelector('.seller__list');
+const buttonListener = (selectorName) => {
+  const buttons = document.querySelector(selectorName);
 
-buttons.addEventListener('click', e => {
-  const btn = e.target.closest('button');
-  if (!btn || !buttons.contains(btn)) return;
+  buttons.addEventListener("click", (e) => {
+    const btn = e.target.closest("button");
+    if (!btn || !buttons.contains(btn)) return;
 
-  if (btn.classList.contains('active')) return;
+    if (btn.classList.contains("active")) return;
 
-  buttons.querySelectorAll('button.active').forEach(b => b.classList.remove('active'));
-  btn.classList.add('active');
+    buttons
+      .querySelectorAll("button.active")
+      .forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+  });
+};
+
+buttonListener(".seller__list");
+buttonListener(".sale__list");
+
+const brandsNames = Array.from(
+  { length: 18 },
+  (_, i) => "./img/brands/" + (i + 1) + ".png"
+);
+
+const brandsList = document.querySelector(".brands__list");
+
+brandsNames.forEach((b) => {
+  const li = document.createElement("li");
+  li.classList.add("brands__item");
+  const img = document.createElement("img");
+  img.classList.add("brands__logo");
+  img.setAttribute("src", b);
+  li.appendChild(img);
+
+  brandsList.appendChild(li);
 });
